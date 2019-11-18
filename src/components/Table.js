ba8677d2d17b71c0f22555419e9456b12c691770
Yoovin <soo22839@gmail.com>
2019-11-18 21:47:45 +0900

    리액트 애니메이션 테스트

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Animated} from "react-animated-css";
 
 export default class Table extends Component {
 
@@ -6,10 +7,38 @@ export default class Table extends Component {
         page:1
     }
 
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <table>
+    //                 <tr>
+    //                     <th>Id</th>
+    //                     <th>Author</th>
+    //                     <th>Title</th>
+    //                     <th>Created</th>
+    //                 </tr>
+    //             {this.props.boards ? this.props.boards.map((row)=>{
+    //                 return(
+    //                 <tr>
+    //                     <td>{row.id}</td>
+    //                     <td>{row.author}</td>
+    //                     <td onClick={()=>this.props.history.push({
+    //                         pathname:`/${this.props.cate}/post/${row.id}`,
+    //                     })}>{row.title}</td>
+    //                     <td>{row.createdAt}</td>
+    //                 </tr>
+    //                 )
+    //             }):'Loading'}
+
+    //             </table>
+    //         </div>
+    //     )
+    // }
     render() {
-        console.log(this.props.boards)
         return (
-            <div>
+            <Animated animationIn="fadeInLeft" animationOut="zoomOutDown" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+                 <div>
                 <table>
                     <tr>
                         <th>Id</th>
@@ -32,6 +61,7 @@ export default class Table extends Component {
 
                 </table>
             </div>
+            </Animated>
         )
     }
 }
